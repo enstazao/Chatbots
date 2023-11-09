47f0442e3de04b9b07e0bf790a73e9b527e5525c
enstazao <p200165@pwr.nu.edu.pk>
2023-11-09 16:04:28 +0500

    created basic chatbot using google palm

---
+++ chatbot-using-google-palm/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from langchain_helper import create_vector_db, get_qa_chain
+
+st.title("Chatbot")
+
+btn = st.button("Create Knowledgebase")
+
+if btn:
+    pass
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_qa_chain()
+    response = chain(question)
+    
+    st.header("Answer: ")
+    st.write(response["result"])
